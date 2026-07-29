41569469f5691350dded4621d8522d19f1b2c034
ZHANG XU <wuxingyuenan5@gmail.com>
2026-07-30 01:40:32 +0800

    feat(research): append macro expectations to legacy macro board

---
+++ admin-risk/src/views/hedgeBoard/macro/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="macro-board-page">
+    <LegacyHedgeBoard />
+    <MacroExpectationPanel />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import LegacyHedgeBoard from '../index.vue';
+  import MacroExpectationPanel from './MacroExpectationPanel.vue';
+</script>
+
+<style scoped lang="less">
+  .macro-board-page { min-width: 0; }
+</style>
