3cf9853b40fb4bc8ef0872c6a98c3cdb4d936a5b
ZHANG XU <wuxingyuenan5@gmail.com>
2026-07-30 02:33:08 +0800

    chore(research): align explicit model event listener lint

--- admin-risk/.eslintrc.js
+++ admin-risk/.eslintrc.js
@@ -5,4 +5,13 @@ module.exports = {
     'no-undef': 'off',
     '@typescript-eslint/no-unused-vars': [1],
   },
+  overrides: [
+    {
+      files: ['src/views/hedgeBoard/aShare/index.vue'],
+      rules: {
+        // CompactSegmentTabs declares the explicit Vue model event name `update:modelValue`.
+        'vue/v-on-event-hyphenation': 'off',
+      },
+    },
+  ],
 };
